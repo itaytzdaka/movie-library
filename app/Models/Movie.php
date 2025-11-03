c735eafbf28514efe2389a9c7e86092a537cc7d8
itaytzdaka <itaytzdaka1@gmail.com>
2025-11-03 19:38:43 +0200

    add movies migration + model

---
+++ app/Models/Movie.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $fillable = [
+        'title',
+        'release_date',
+        'poster_url',
+        'director',
+        'runtime_minutes',
+        'actors',
+        'genre',
+    ];
+}
---
+++ database/migrations/2025_11_03_170713_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->date('release_date')->nullable();
+            $table->string('poster_url', 500)->nullable();
+            $table->string('director', 100)->nullable();
+            $table->unsignedInteger('runtime_minutes')->nullable();
+            $table->text('actors')->nullable();
+            $table->string('genre')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
