f64c82728cf0953381a7d24a096776bae4a8ea1c
itaytzdaka <itaytzdaka1@gmail.com>
2025-11-06 21:37:47 +0200

    add responsive css code

--- resources/views/components/genres.blade.php
+++ resources/views/components/genres.blade.php
@@ -10,15 +10,13 @@
 
 <form id="genre-form" method="get" action="{{ route('movies.index') }}">
 
-    <div>
-        @php $selected = (array)($selected ?? []); @endphp
-        @foreach($genres as $g)
-            <label class="genre-toggle">
-            <input type="checkbox" class="genre-check" value="{{ $g->name }}" name="genre[]"
-                @checked(in_array($g->name, $selected))>
-                <span>{{ $g->name }}</span>
-            </label>
-        @endforeach
-    </div>
+  @php $selected = (array)($selected ?? []); @endphp
+  @foreach($genres as $g)
+      <label class="genre-toggle">
+      <input type="checkbox" class="genre-check" value="{{ $g->name }}" name="genre[]"
+          @checked(in_array($g->name, $selected))>
+          <span>{{ $g->name }}</span>
+      </label>
+  @endforeach
 
 </form>
